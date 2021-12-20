45ac72f7eb1953b7e5732437975d4bc1f0f70d94
edvinsyk <edvin.syk@gmail.com>
2021-12-20 18:56:39 +0100

    added first function + desc

--- recode_function.R
+++ recode_function.R
@@ -1,9 +1,26 @@
 
-### Recode function ### 
+#' A recode-function that updates the variable by reference. Requires data.table.
+#' Prints the count of the recoded variable.
+#' @param dt Working data.table
+#' @param variable The variable to recode
+#' @param old a vector of the old values
+#' @param new a vector of the new values
+#' @keywords recode, lookup
+#' @export
+#' @examples
+#' e_recode()
+e_recode <- function(dt, variable, old, new) {
+require(data.table)
 
-library(data.table)
+dt <- dt
+old <- c(old)
+new <- c(new)
+x <- as.character(variable)
 
-dt <- data.table(x = sample(1:10, 500, replace = TRUE), y = sample(1:10, 500, replace = TRUE))
+lookup <- data.frame(old, new)
 
+dt[lookup, on = c(x = "old"), x := new]
 
+print(dt[, .N, keyby = variable])
 
+}
